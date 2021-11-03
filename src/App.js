25b329a7a5d1464e6b69e7d568040d8884d58754
DeepaThangaraju <deepat2405@gmail.com>
2021-11-03 03:25:03 -0700

    pricingtable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
+export default function App() {
+  const mod=[
+    {
+      type:"FREE",
+      price:"$0",
+      user:"✔ Single User",
+      storage:"✔ 5GB Storage",
+      pubproject:"✔ Unlimited Public Projects",
+      community:"✔ Comminty Access",
+      priproject:"❌ Unlimited Private project",
+      support:"❌ Dedicated Phone Support",
+      domain:"❌ Free Subdomain",
+      status:"❌ Monthly Status Support",
+    },
+    {
+      type:"PLUS",
+      price:"$9",
+      user:"✔ 5 User",
+      storage:"✔ 50GB Storage",
+      pubproject:"✔ Unlimited Public Projects",
+      community:"✔ Comminty Access",
+      priproject:"✔ Unlimited Private project",
+      support:"✔ Dedicated Phone Support",
+      domain:"✔ Free Subdomain",
+      status:"❌ Monthly Status Support",
+    },
+    {
+      type:"PRO",
+      price:"$49",
+      user:"✔ Unlimited User",
+      storage:"✔ 150GB Storage",
+      pubproject:"✔ Unlimited Public Projects",
+      community:"✔ Comminty Access",
+      priproject:"✔ Unlimited Private project",
+      support:"✔ Dedicated Phone Support",
+      domain:"✔ Unlimited Free Subdomain",
+      status:"✔ Monthly Status Support",
+    },
+  ]
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       {mod.map(obj=> <Print type={obj.type} price={obj.price} user={obj.user} storage={obj.storage} 
+       pubproject={obj.pubproject} community={obj.community} priproject={obj.priproject} support={obj.support}
+       domain={obj.domain} status={obj.status}
+        />)}
+        
     </div>
-  );
+  )
 }
 
-export default App;
+function Print({type,price,user,storage,pubproject,community,priproject,support,domain,status}){
+  const dis={opacity:0.5};
+  const bld={"font-weight":"bold"};
+  if(type==="PRO"){
+    return (
+  
+    <div className="phone">
+      <div className="space">
+        
+           <h4>{type}</h4>
+           <h1>{price}</h1>/month<hr></hr>
+           <p style={bld}>{user}</p>
+           <p>{storage}</p>
+           <p>{pubproject}</p>
+           <p>{community}</p>
+           <p >{priproject}</p>
+           <p>{support}</p>
+           <p>{domain}</p>
+           <p>{status}</p>
+        </div>
+           <button>BUTTON</button>
+    </div>
+  )
+    }
+    else if(type==="FREE"){
+      return (
+  
+        <div className="phone">
+          <div className="space">
+            
+               <h4>{type}</h4>
+               <h1>{price}</h1>/month<hr></hr>
+               <p>{user}</p>
+               <p>{storage}</p>
+               <p>{pubproject}</p>
+               <p>{community}</p>
+               <p style={dis}>{priproject}</p>
+               <p style={dis}>{support}</p>
+               <p style={dis}>{domain}</p>
+               <p style={dis}>{status}</p>
+            </div>
+               <button>BUTTON</button>
+        </div>
+      )
+    }
+    else if(type==="PLUS"){
+      return (
+  
+        <div className="phone">
+          <div className="space">
+            
+               <h4>{type}</h4>
+               <h1>{price}</h1>/month<hr></hr>
+               <p style={bld}>{user}</p>
+               <p>{storage}</p>
+               <p>{pubproject}</p>
+               <p>{community}</p>
+               <p>{priproject}</p>
+               <p>{support}</p>
+               <p>{domain}</p>
+               <p style={dis}>{status}</p>
+            </div>
+               <button>BUTTON</button>
+        </div>
+      )
+    }
+}
+ 
